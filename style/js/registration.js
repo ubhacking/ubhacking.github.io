e965c7e6ffef6dd124e9e9084909a4b83471f7d0
jared <jaredvar@buffalo.edu>
2015-07-29 15:12:35 -0400

    javascript and registration

--- style/js/registration.js
+++ style/js/registration.js
@@ -2,52 +2,70 @@ $(document).ready(function() {
 	jsonToDatalist('http://localhost:5000/colleges', '#colleges');
 	jsonToDatalist('http://localhost:5000/majors', '#majors');
 
-	/*// process the form
+	// process the form: sends json of everything except resume via AJAX and separately uploads
+	// file.
 	$('form').submit(function(event) {
 
 		// get the form data
 		// there are many ways to get this data using jQuery (you can use the class or id also)
 		var formData = {
-			'name' 				: $('input[name=email]').val(),
-			'email' 			: $('input[name=password]').val()
+			'email' 			: $('input[name=email]').val(),
+			'password'			: $('input[name=password').val(),
+			'confirm-password'	: $('input[name=confirm-password').val(),
+			'firstname' 		: $('input[name=firstname]').val(),
+			'lastname'			: $('input[name=lastname]').val(),
+			'phone'				: $('input[name=phone]').val(),
+			'sex'				: $('#sex :selected').val(),
+			'school'			: $('input[name=colleges]').val(),
+			'major'				: $('input[name=major]').val(),
+			'year'				: $('#year :selected').val(),
+			'first-hackathon'	: $('#first-hackathon option:selected').val(),
+			'project-type'		: $('#project-type :selected').val(),
+			'project'			: $('textarea#project').val(),
+			'past-project'		: $('textarea#past-project').val(),
+			'github'			: $('input[name=github]').val(),
+			'linkedin'			: $('input[name=linkedin]').val(),
+			'website'			: $('input[name=website]').val(),
+			'diet'				: $('#diet :selected').val(),
+			'shirt'				: $('#shirt :selected').val(),
+			'other'				: $('textarea#other').val()
 		};
+
+		console.log(formData);
 
 		// process the form
 		$.ajax({
 			type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-			url 		: 'localhost:5000/submit', // the url where we want to POST
+			url 		: 'http://localhost:5000/submit', // the url where we want to POST
 			data 		: formData, // our data object
-			dataType 	: 'json', // what type of data do we expect back from the server
+			dataType 	: 'application/json', // what type of data do we expect back from the server
 			encode 		: true
 		})
 			// using the done promise callback
 			.done(function(data) {
 
 				// log data to the console so we can see
-				console.log(data); 
-
+					console.log(data); 
+				
 				// here we will handle errors and validation messages
 				if ( ! data.success) {
+					alert("fail");
 					
 					// handle errors for name ---------------
-					if (data.errors.name) {
-						$('#name-group').addClass('has-error'); // add the error class to show red input
-						$('#name-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
+					if (data.errors.firstname) {
+						alert(data.errors.firstname)
+						
 					}
-
-					// handle errors for email ---------------
+					
+					/*
+					// handle errors for email --------------- EXAMPLE
 					if (data.errors.email) {
 						$('#email-group').addClass('has-error'); // add the error class to show red input
 						$('#email-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
-					}
-
-					// handle errors for superhero alias ---------------
-					if (data.errors.superheroAlias) {
-						$('#superhero-group').addClass('has-error'); // add the error class to show red input
-						$('#superhero-group').append('<div class="help-block">' + data.errors.superheroAlias + '</div>'); // add the actual error message under our input
-					}
+					}*/
 
 				} else {
+					alert("success");
 
 					// ALL GOOD! just show the success message!
 					//$('form').append('<div class="alert alert-success">' + data.message + '</div>');
@@ -68,10 +86,11 @@ $(document).ready(function() {
 
 		// stop the form from submitting the normal way and refreshing the page
 		event.preventDefault();
-	});*/
+	});
 
 });
 
+//method used to populate datalists from a JSON array
 function jsonToDatalist(url, id) {
 	$.getJSON(url, function(data) {
 		var options;
