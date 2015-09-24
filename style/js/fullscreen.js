fccfe94ab72f95042f94b0df1580018e6d8d167a
Gela <gelarehm@buffalo.edu>
2015-09-23 21:57:38 -0400

    Fixed nav menu, it now actually works and changes color on hover

--- style/js/fullscreen.js
+++ style/js/fullscreen.js
@@ -5,3 +5,8 @@ $(document).ready(function() {
   var element = $("#fullscreen");
   element.height(height - $(".container").height());
 });
+
+$('.nav a').on('click', function(){
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+});
