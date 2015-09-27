1f31ec779dd4ec0abf94ba4287612466b3aee8f2
Alexander Rookey <atrookey@buffalo.edu>
2015-09-27 17:36:59 -0400

    wrote some js to get sponsor page working

--- style/js/sponsor.js
+++ style/js/sponsor.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
     $('#resume-table').DataTable({
-      "ajax" : '/sponsors/view_all'
+      "ajax" : '/sponsors/view_all',
+      "columnDefs": [
+            {
+                "targets": [ 0 ],
+                "visible": false,
+                "searchable": false
+            }
+        ],
+      "createdRow": function ( row, data, index ) {
+        $('td', row).click(function() {
+          viewResume(data[0]);
+        });
+      }
     });
+
+    function viewResume(uid) {
+      window.location.href = "/sponsors/view_resume?uid=" + uid;
+    }
 });
+
