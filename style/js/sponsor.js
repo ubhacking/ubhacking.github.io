dbc5325f88d026d64af488695a578cb4c16670d5
Zach Wieand <zmwieand@gmail.com>
2015-10-13 00:23:36 -0400

    adding in sponsors javascript

--- style/js/sponsor.js
+++ style/js/sponsor.js
@@ -1,5 +1,27 @@
+// $(document).ready(function(){
+//     $('#resume-table').DataTable({
+//       "ajax" : '/sponsors/view_all',
+//       "columnDefs": [
+//             {
+//                 "targets": [ 0 ],
+//                 "visible": false,
+//                 "searchable": false
+//             }
+//         ],
+//       "createdRow": function ( row, data, index ) {
+//         $('td', row).click(function() {
+//           viewResume(data[0]);
+//         });
+//       }
+//     });
+
+//     function viewResume(uid) {
+//       window.location.href = "/sponsors/view_resume?uid=" + uid;
+//     }
+// });
+
 $(document).ready(function(){
-    $('#resume-table').DataTable({
+    var table = $('#resume-table').DataTable({
       "ajax" : '/sponsors/view_all',
       "columnDefs": [
             {
@@ -7,12 +29,28 @@ $(document).ready(function(){
                 "visible": false,
                 "searchable": false
             }
-        ],
-      "createdRow": function ( row, data, index ) {
-        $('td', row).click(function() {
-          viewResume(data[0]);
-        });
-      }
+        ]
+    });
+
+    $('#resume-table tbody').on( 'click', 'tr', function () {
+        $(this).toggleClass('selected');
+    });
+
+    $('#select-all').click(function () {
+      $('#resume-table tbody > tr').addClass('selected');
+    });
+
+    $('#select-none').click(function () {
+      $('#resume-table tbody > tr').removeClass('selected');
+    });
+
+    $('#download').click(function () {
+      uids = [];
+      table.rows( {selected:true} ).each(function() {
+        uids.push(table.row(this).data()[0]);
+      });
+      console.log(uids);
+      //window.location.href = "http://stackoverflow.com";
     });
 
     function viewResume(uid) {
